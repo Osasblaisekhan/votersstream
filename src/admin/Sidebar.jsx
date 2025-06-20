0f0c7b11f14f42a01c532d9899ab5425eb464271
NGUITCHOU <nguitchoumurielle@gmail.com>
2025-06-20 17:13:02 +0100

    make changes

--- src/admin/Sidebar.jsx
+++ src/admin/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
   return (
     <div>
     <nav className='text-white mt-6 mb-6'>
-      <ul className='flex flex-col gap-6  items-center'>
+      <ul className='flex flex-col gap-12  items-center'>
         
        <li className='w-40 h-9 text-center border-b-2 hover:text-amber-700 cursor-pointer hover:bg-amber-100'> <Link to={'/'}>Home</Link></li>
        <li className='w-40 h-9 text-center border-b-2 hover:text-amber-700 cursor-pointer hover:bg-amber-100'> <Link to={'/dashboard'}>Dashboard</Link></li>
@@ -15,19 +15,7 @@ const Sidebar = () => {
        <li className='w-40 h-9 text-center border-b-2 hover:text-amber-700 cursor-pointer hover:bg-amber-100'> <Link to={'/viewCampaigns'}>View Campaigns</Link></li>
        <li className='w-40 h-9 text-center border-b-2 hover:text-amber-700 cursor-pointer hover:bg-amber-100'> <Link to={'/statistics'}>Statistics</Link></li>
 
-        <li className='w-40 h-9 text-center border-b-2'>Home</li>
-        <li className='w-40 h-9 text-center border-b-2'>Dashbord</li>
-        <li className='w-40 h-9 text-center border-b-2'>Create Campaigns</li>
-        <li className='w-40 h-9 text-center border-b-2'>Creat Contestants</li>
-        <li className='w-40 h-9 text-center border-b-2'>View Campaigns</li>
-        <li className='w-40 h-9 text-center border-b-2'>Statistics</li>
-
-        <li className='w-40 h-9 text-center border-b-2'>Home</li>
-        <li className='w-40 h-9 text-center border-b-2'>Dashbord</li>
-        <li className='w-40 h-9 text-center border-b-2'>Create Campaigns</li>
-        <li className='w-40 h-9 text-center border-b-2'>Creat Contestants</li>
-        <li className='w-40 h-9 text-center border-b-2'>View Campaigns</li>
-        <li className='w-40 h-9 text-center border-b-2'>Statistics</li>
+       
       </ul>
     </nav>
     </div>
